11e34b8bdecef22366d6a5742b93b6fe8e470bea
ggunG <ggung0929@gmail.com>
2023-08-16 11:00:36 +0900

    8.16 gitignore적용

---
+++ springProj/src/main/java/aop_p/webToon/JoSuk.java
@@ -0,0 +1,17 @@
+package aop_p.webToon;
+
+public class JoSuk {
+	public String dog1() {
+		System.out.println("센세이션은 찢찢");
+		return "센세이션";
+	}
+	public String wife() {
+		System.out.println("애봉파워");
+		return "애봉";
+	}
+	
+	public int papa() {
+		System.out.println("조철왕");
+		return 4000;
+	}
+}
